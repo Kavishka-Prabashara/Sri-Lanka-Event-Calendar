117c868615cf93f33703abccc2de09365232eab0
Kavishka-Prabashara <kavishkaprabashara@gmail.com>
2025-04-18 22:49:23 +0530

    configure project for deployment

---
+++ src/components/navBar.tsx
@@ -0,0 +1,19 @@
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                    Sri Lanka Event Calendar
+                </Typography>
+                <Button color="inherit" component={Link} to="/">Home</Button>
+                <Button color="inherit" component={Link} to="/about">About</Button>
+                <Button color="inherit" component={Link} to="/helps">Helps</Button>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default NavBar;
---
+++ src/pages/helps.tsx
@@ -0,0 +1,5 @@
+const Helps = () => {
+    return <h1>Need Help?</h1>;
+};
+
+export default Helps;
