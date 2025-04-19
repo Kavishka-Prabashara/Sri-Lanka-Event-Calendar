3bb4ecbada4c5d898b628603fb13b23a362ba4bf
Kavishka-Prabashara <kavishkaprabashara@gmail.com>
2025-04-19 19:15:42 +0530

    Update home

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(
     {
-      base: '/Sri-Lanka-Event-Calendar/', // VERY IMPORTANT
+      base: '/home/', // VERY IMPORTANT
       plugins: [react()],
 })
