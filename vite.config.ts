024bb184a1d6de1d8a1376a70c36a7f4e5a96df5
Kavishka-Prabashara <kavishkaprabashara@gmail.com>
2025-04-20 20:20:37 +0530

    update PostCSS config to use @tailwindcss/postcss plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig(
-    {
-      base: '/Sri-Lanka-Event-Calendar/', // VERY IMPORTANT
-      plugins: [react()],
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+      plugins: [
+            tailwindcss(),
+      ],
 })
