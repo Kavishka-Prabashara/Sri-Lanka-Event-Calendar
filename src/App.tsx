21a6621f8ea1d6403db2c109492de20e70a91e94
Kavishka-Prabashara <kavishkaprabashara@gmail.com>
2025-04-22 23:37:20 +0530

    Refactor routing to scroll to About section from Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import NavBar from './components/navBar';
 import Home from './pages/home';
 import About from './pages/about';
 import Helps from './pages/helps';
 import './App.css';
 
+function HomeWithAbout() {
+    return (
+        <>
+            <div id="home">
+                <Home />
+            </div>
+            <div id="about">
+                <About />
+            </div>
+        </>
+    );
+}
+
 function App() {
     return (
         <Router>
             <NavBar />
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
+                <Route path="/" element={<HomeWithAbout />} />
                 <Route path="/helps" element={<Helps />} />
             </Routes>
         </Router>
